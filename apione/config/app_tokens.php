1f87f60a6d7380711132dc93c3bc0e27cff3f467
pcualMac <jfpum.cal@gmail.com>
2024-05-09 14:05:45 +0100

    config

---
+++ apione/config/app_tokens.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'jwt_secret' => 'LC0JxIPDeSQbQN0Nk5UDjW1fG2m2CF83AfeTiNVndlaJrgSLJ8VYHeynsXXDMugo',
+    'application1' => env('JWT_SECRET_APP1', 'default_secret_key_for_app1'),
+    'application2' => env('JWT_SECRET_APP1', 'default_secret_key_for_app1'),
+    // Add more applications as needed...
+];
